e3ada222bc832118b970e7708cff598cb2309c86
Philip Lee <github@pjlconsultants.co.uk>
2021-04-03 17:05:17 +0100

    Moved to modern csproj file format. Now uses Unity3d nuget package.

--- SolastaMoveAllRooms/Properties/AssemblyInfo.cs
+++ SolastaMoveAllRooms/Properties/AssemblyInfo.cs
@@ -1,17 +1,8 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SolastaMoveAllRooms")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SolastaMoveAllRooms")]
-[assembly: AssemblyCopyright("Copyright © ImpPhil 2021")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -28,5 +19,3 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.8.0")]
-[assembly: AssemblyFileVersion("1.0.8.0")]
